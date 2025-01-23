2d2c05f408e415e3f4458cccfc284efc91b3aab5
pushkar raj <pushkarraj192003@gmail.com>
2025-01-23 14:32:46 +0530

    Update permissions.py

--- login/permissions.py
+++ login/permissions.py
@@ -7,8 +7,9 @@ class IsAdminOrReadOnly(BasePermission):
     """
     def has_permission(self, request, view):
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
-            return True  # Allow read-only actions for all users
+            return request.user and request.user.is_authenticated  # Allow read-only actions for all users
         return request.user.is_staff  # Only admins can perform write actions
+    
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_superuser
